776dad1e5b61cf9d4fb7c749cc210294bebfb675
Fernando Basso <fernandobasso.br@gmail.com>
2014-10-30 19:17:07 -0200

    Estado DAO

--- src/java/br/ifsul/edu/dao/GenericDAO.java
+++ src/java/br/ifsul/edu/dao/GenericDAO.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.ifsul.edu.dao;
 
 import br.ifsul.edu.converters.ConverterOrder;
@@ -12,8 +8,6 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
-
-
 
 //
 // O <T> indica que é genérica.
